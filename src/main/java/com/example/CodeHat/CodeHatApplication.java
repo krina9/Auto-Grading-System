63a1b225bf1c30e868b87fdbd43d9e48e12a02da
Trusha Nipul Shah <tr335214@dal.ca>
2022-10-03 18:25:24 -0300

    Setting up CICD

--- src/main/java/com/example/CodeHat/CodeHatApplication.java
+++ src/main/java/com/example/CodeHat/CodeHatApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CodeHatApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CodeHatApplication.class, args);
 	}
---
+++ src/main/java/com/example/CodeHat/Controller.java
@@ -0,0 +1,13 @@
+package com.example.CodeHat;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String test(){
+        return "This works!";
+    }
+}
