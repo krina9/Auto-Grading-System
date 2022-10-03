7c9aed3f58acc4d884b8153bcf684890aecf9e28
Trusha Nipul Shah <tr335214@dal.ca>
2022-10-03 17:58:10 -0300

    Created an empty project.

---
+++ src/main/java/com/example/CodeHat/CodeHatApplication.java
@@ -0,0 +1,13 @@
+package com.example.CodeHat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeHatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeHatApplication.class, args);
+	}
+
+}
