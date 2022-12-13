9a0d898a4143128bc2ef8df0fc4a2614d1258422
Jainisha Jayamurthy <jn278522@dal.ca>
2022-12-12 23:30:22 -0400

    all team members - refactored in single laptop to avoid merge issues.

--- src/main/java/com/example/AutoGrad/Controllers/AuthenticationController.java
+++ src/main/java/com/example/AutoGrad/Controllers/AuthenticationController.java
@@ -1,13 +1,12 @@
 package com.example.AutoGrad.Controllers;
 
-import com.example.AutoGrad.Services.IAuthenticationService;
-import com.example.AutoGrad.Services.IInstructorService;
-import com.example.AutoGrad.Services.Impl.AuthenticationService;
-import com.example.AutoGrad.Services.Impl.InstructorService;
-import com.example.AutoGrad.Services.dto.ChangePasswordDTO;
-import com.example.AutoGrad.Services.dto.LoginDTO;
-import com.example.AutoGrad.Services.dto.UserDTO;
-import org.springframework.context.annotation.Lazy;
+import com.example.AutoGrad.Model.User;
+import com.example.AutoGrad.dataLayer.IAuthentication;
+import com.example.AutoGrad.Model.dto.ChangePasswordDTO;
+import com.example.AutoGrad.Model.dto.LoginDTO;
+import com.example.AutoGrad.Model.dto.UserDTO;
+import com.example.AutoGrad.dataLayer.IUser;
+import com.example.AutoGrad.dataLayer.dao.UserDAO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,30 +14,41 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 public class AuthenticationController {
 
+    UserDAO userDAO = new UserDAO();
 
-    @Lazy
-    private IAuthenticationService authenticationService = new AuthenticationService();
-    private IInstructorService instructorService = new InstructorService();
+    IAuthentication authenticationService = new User(userDAO);
+
+    IUser userService = new User(userDAO);
 
     @GetMapping("/api/confirm-account")
     public ResponseEntity activate(@RequestParam("token") String token) {
-        return authenticationService.activateAccount(token);
+        User user = authenticationService.activateAccount(token);
+        if(user!=null) {
+            return ResponseEntity.ok().body(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping("/api/signup")
     public ResponseEntity signup(@RequestBody UserDTO userDTO) {
-        return authenticationService.addUser(userDTO);
+        User user = userService.addUser(userDTO);
+        if(user != null) {
+            return ResponseEntity.ok().body(user);
+        } else{
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
 
     @PostMapping("/api/login")
     public ResponseEntity login(@RequestBody LoginDTO credentials) {
-        return authenticationService.login(credentials);
-    }
-
-    @PostMapping("/api/user/change-password")
-    public ResponseEntity updatePwd(@RequestBody ChangePasswordDTO data) {
-        return instructorService.updatePassword(data);
+        User user = authenticationService.login(credentials);
+        if(user!=null){
+            return ResponseEntity.ok().body(user);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
 }
